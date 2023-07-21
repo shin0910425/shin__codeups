e897e278e2724521f394815ac7300e1e81b57359
shin0910425 <shin0910425@gmail.com>
2023-07-21 10:37:23 +0900

    0721-1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  let topBtn = $('.to-top');
+  topBtn.hide();
 
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass('is-active')) {
+      $('.js-hamburger').removeClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $('.js-hamburger').addClass("is-active");
+      // $("html").toggleClass("is-fixed");
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 });
