b85500ef7d70b111cae26ca9c338832416987cbd
shin0910425 <shin0910425@gmail.com>
2023-11-12 23:07:29 +0900

    1112

--- src/js/script.js
+++ src/js/script.js
@@ -223,3 +223,10 @@ jQuery(function ($) {
     $('#' + number).addClass('is-active');
   });
 });
+
+jQuery(function ($) {
+  $('.js-faq-question').on('click', function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass('is-open');
+  });
+});
